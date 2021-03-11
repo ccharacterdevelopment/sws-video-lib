a70ccff238f8f8508310cbb2e2acdde394b5604e
Sojourn4 <58755132+Sojourn4@users.noreply.github.com>
2021-03-11 11:36:48 -0500

    vid-lib

--- sws-video-lib.php
+++ sws-video-lib.php
@@ -1,8 +1,8 @@
 <?php
 /**s
- * Plugin Name:       SWS Directory/Interactive Map
- * Plugin URI:        https://ccharacter.com/custom-plugins/sws-dir-map/
- * Description:       Builds on the DBI to display interactive map/directory
+ * Plugin Name:       SWS Video Library
+ * Plugin URI:        https://ccharacter.com/custom-plugins/sws-video-lib/
+ * Description:       Video lib
  * Version:           1.3
  * Requires at least: 5.2
  * Requires PHP:      5.5
@@ -10,16 +10,16 @@
  * Author URI:        https://ccharacter.com/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       sws-dir-map
+ * Text Domain:       sws-video-lib
  */
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 require_once plugin_dir_path(__FILE__).'inc/plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'https://raw.githubusercontent.com/ccharacter/sws-dir-map/main/plugin.json',
+	'https://raw.githubusercontent.com/ccharacter/sws-video-lib/main/plugin.json',
 	__FILE__,
-	'sws_dir_map'
+	'sws-video-lib'
 );
 
 //require_once plugin_dir_path(__FILE__)."inc/dir/assets/Db.php";
@@ -28,7 +28,7 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 
 
 // SHORTCODE FOR directories  
-function sws_dir_show($atts) {
+/*function sws_dir_show($atts) {
 	
 	$vars=array();
 	
@@ -131,7 +131,7 @@ iframe {
 	width: 1px;
 	min-width: 100%;
 }
-</style>";*/
+</style>";
 	if ($vars['group_by_conf']=="Y") { 
 		echo "<iframe id='myIframe' src='".plugins_url( '/inc/dir/dir_unions_ejj.php?vars='.$getVar, __FILE__ )."' style='width: 100%; height:1000px; min-height: 50em;' frameborder='no' scrolling='no'></iframe>"; 
 	} else {
@@ -145,6 +145,6 @@ iframe {
 
 // register shortcode
 add_shortcode('sws_dir_photo', 'ejj_photo_dir');
-
+*/
 ?>
 
